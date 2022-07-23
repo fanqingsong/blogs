54fd85c3afba1fd8d67317a1a7752c5fe2c35765
lightsong <qsfan@qq.com>
2022-07-23 23:20:44 +0800

    update

--- backend/src/api/testcollection/controllers/testcollection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  testcollection controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::testcollection.testcollection');
--- backend/src/api/testcollection/services/testcollection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * testcollection service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::testcollection.testcollection');
